7f129cd9392bca4e387c90d3e74537babf69497d
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-12 09:26:10 +0530

    add view engine

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 8000;
 
 app.use('/', require('./routes/index')); // we can do './routes' also s it will automatically fetch index
 
+// set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err)
         console.log(`Error in running server: ${err}`);
