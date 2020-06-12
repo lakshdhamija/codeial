162330080f3c44376f3a82e53f0952bb0de0afd8
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-12 09:36:42 +0530

    added view files for home and user profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile = function(req, res){
-    res.end('<h1>User Profile</h1>');
+    return res.render('users_profile', {
+        title: "Codeial",
+        name: "Laksh"
+    });
 }
