9f45287d55298160de04483fa1da80769aa01422
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-12 09:41:38 +0530

    added route, controller and viw for users posts

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,11 @@ module.exports.profile = function(req, res){
         name: "Laksh"
     });
 }
+
+module.exports.posts = function(req, res){
+    return res.render('users_posts', {
+        title: "Codeial",
+        name: "Laksh",
+        color: "Red"
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
+router.get('/posts', usersController.posts);
 
 module.exports = router;
