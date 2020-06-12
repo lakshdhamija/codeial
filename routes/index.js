bdc6be4107a1a19962e418e1057d8c15c399d012
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-12 09:22:15 +0530

    add another route and controller for /users/profile

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users')); // require users file if incoming request is /users
 
 module.exports = router; // exporting to make available to main index.js
