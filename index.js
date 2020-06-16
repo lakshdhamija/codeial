b6f06c95bbe03b63fa200b6bf24d4bbf19a9664d
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-16 10:48:29 +0530

    connected db

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.static('./assets'));
 
 app.use(expressLayouts);
+
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use('/', require('./routes/index')); // we can do './routes' also s it will automatically fetch index
 
