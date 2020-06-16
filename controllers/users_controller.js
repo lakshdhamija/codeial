0ff814913aa6f03587113e58ee3dec53d16347dc
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-16 12:27:14 +0530

    added forms to sign in and sign up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('users_profile', {
         title: "Codeial",
@@ -16,15 +18,23 @@ module.exports.posts = function(req, res){
 //render the sign up page
 module.exports.signUp = function(req, res){
     return res.render('user_sign_up',{
-        title: "Codeial | Sign Up",
-
+        title: "Codeial | Sign Up"
     });
 }
 
 //Render the sign in page
 module.exports.signIn = function(req, res){
     return res.render('user_sign_in',{
-        title: "Codeial | Sign In",
-
+        title: "Codeial | Sign In"
     });
+}
+
+//get the sign up data
+module.exports.create = function(req, res){
+    //TODO later
+}
+
+//sign in and create session for user
+module.exports.createSession = function(req, res){
+    //TODO later
 }
