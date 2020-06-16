d9d98bce18fc9bd0ffadc823bc5bc6e39d2f065b
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-16 12:47:36 +0530

    added functionality to sign up in users_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -31,7 +31,26 @@ module.exports.signIn = function(req, res){
 
 //get the sign up data
 module.exports.create = function(req, res){
-    //TODO later
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({ email: req.body.email }, function(err, user){
+        if(err){
+            console.log('error in finding user in signing up');
+            return;
+        }
+        if(!user){ // if user doesn't exist, create user
+            User.create(req.body, function(err, user){
+                if(err){
+                    console.log('error in creating user while signing up');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            });
+        }else{ // if user exists then redirect to sign up page
+            return res.redirect('back');     
+        }
+    });
 }
 
 //sign in and create session for user
--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,6 @@ router.get('/profile', usersController.profile);
 router.get('/posts', usersController.posts);
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
+router.post('/create', usersController.create);
 
 module.exports = router;
