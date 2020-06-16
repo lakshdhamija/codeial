a743dcc3ef190eed99dc3ad1ae1d0de4991c462d
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-16 11:04:46 +0530

    added base for sign in and sin up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -12,3 +12,19 @@ module.exports.posts = function(req, res){
         color: "Red"
     });
 }
+
+//render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up",
+
+    });
+}
+
+//Render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In",
+
+    });
+}
