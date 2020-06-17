670f40198ad63fbf619fa3188b0ed972c4272210
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-17 20:48:52 +0530

    added profile info on profile page and permanent storage for session cookies using connect mongo

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -17,6 +17,9 @@ module.exports.posts = function(req, res){
 
 //render the sign up page
 module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up',{
         title: "Codeial | Sign Up"
     });
@@ -24,6 +27,9 @@ module.exports.signUp = function(req, res){
 
 //Render the sign in page
 module.exports.signIn = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title: "Codeial | Sign In"
     });
@@ -55,5 +61,5 @@ module.exports.create = function(req, res){
 
 //sign in and create session for user
 module.exports.createSession = function(req, res){
-    return res.redirect('/');
+    return res.redirect('/users/profile');
 }
