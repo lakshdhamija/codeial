605e497ba343f9fc3cfc036e5cca028895801960
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-17 20:17:19 +0530

    added checkAuthentication to check if user exists and setAuthenticated User to set the user in locals for the views

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,57 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+
+//authentication using passport
+passport.use(new LocalStrategy({
+        usernameField: 'email'
+    },
+    function(email, password, done){
+        //find user and establish identity
+        User.findOne({email: email}, function(err, user){
+            if(err){
+                console.log('error in finding user --> passport');
+                return done(err);
+            }
+            if(!user || user.password != password){
+                console.log('Invalid Username / Password');
+                return done(null, false);
+            }
+            return done(null, user); // user found so passing on the user
+        });
+    }
+));
+
+//serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function(user, done){
+    done(null, user.id); // here id is automatically encrypted
+});
+
+//deserializing the user from the key in the cookies
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        if(err){
+            console.log('error in finding user --> passport');
+            return done(err);
+        }
+        return done(null, user);
+    });
+});
+
+//check if user is authenticated 
+passport.checkAuthentication = function(req, res, next){
+    if(req.isAuthenticated()){ // if user is signed in
+        return next(); // pass him onto the next function(controller's action)
+    }
+    // if user not signed in
+    return res.redirect('/users/sign-in');
+}
+passport.setAuthenticatedUser = function(req, res, next){
+    if(req.isAuthenticated()){
+        // When we log in through the passport, the user info is already handled by the user under req.user(contains the current signed in user from the session cookie). We pass it onto the response locals ( for the views )
+        res.locals.user = req.user;
+    }
+    next();
+}
+
+module.exports = passport;
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+//used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy'); 
 
 app.use(express.urlencoded());
 
@@ -17,11 +21,28 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-app.use('/', require('./routes/index')); // we can do './routes' also s it will automatically fetch index
+
 
 // set up view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(session({
+    name: 'codeial',
+    //TODO: change secret before deployment in production
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use('/', require('./routes/index')); // we can do './routes' also s it will automatically fetch index
 
 app.listen(port, function(err){
     if(err)
