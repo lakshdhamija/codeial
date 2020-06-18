2475f7a2f6b7a10049dcea7054f474d56a51f308
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-18 17:09:21 +0530

    add post schema and form

---
+++ models/post.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: { // linking to user Schema already in database
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // specifying schema to which we will refer
+        required: true
+    }
+},{
+    timestamps: true
+});
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
