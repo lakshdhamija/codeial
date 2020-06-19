2fa1eae053b0996c5f09493abf58493e46508d57
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-19 23:59:07 +0530

    add posts div at home to display posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,12 @@
+const Posts = require('../models/posts');
 module.exports.home = function(req, res){
-    return res.render('home', {
-        title: "Codeial"
+    //console.log(req.cookies);
+    //res.cookie('user_id', 25);
+    Posts.find({}, function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
     });
+    
 }
