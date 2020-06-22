0faee3e7c40c136b634891e078915fb8380ccdf7
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-22 15:53:28 +0530

    populate post chema with user details to display post author

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,19 @@
-const Posts = require('../models/posts');
+const Posts = require('../models/post');
 module.exports.home = function(req, res){
     //console.log(req.cookies);
     //res.cookie('user_id', 25);
-    Posts.find({}, function(err, posts){
+    // Posts.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // populate user of each post
+    Posts.find({}).populate('user').exec(function(err, posts){
         return res.render('home', {
             title: "Codeial | Home",
             posts: posts
         });
     });
-    
 }
