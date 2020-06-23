bede6bc98d6be8987fd33ea6a894a65161919645
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-23 14:52:19 +0530

    add comment and user who made comment on home.ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -10,7 +10,15 @@ module.exports.home = function(req, res){
     // });
 
     // populate user of each post
-    Posts.find({}).populate('user').exec(function(err, posts){
+    Posts.find({})
+    .populate('user') // populating user in posts to display user name who made post
+    .populate({
+        path: 'comments', // (name specified according to post.js model) populating comments to display comment
+        populate: {
+            path: 'user' // populating user in comments to display user name who made comment
+        }
+    })
+    .exec(function(err, posts){
         return res.render('home', {
             title: "Codeial | Home",
             posts: posts
