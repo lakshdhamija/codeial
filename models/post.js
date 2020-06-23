8c5df149bbc069bd422aef304ca318eb04bc0829
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-23 13:28:37 +0530

    create comments schema and add comment array to posts schema for fast reference

--- models/post.js
+++ models/post.js
@@ -7,9 +7,15 @@ const postSchema = mongoose.Schema({
     },
     user: { // linking to user Schema already in database
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // specifying schema to which we will refer
-        required: true
-    }
+        ref: 'User' // specifying schema to which we will refer
+    },
+    // include array of ids of all comments in the post schema itself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'comment'
+        }
+    ]
 },{
     timestamps: true
 });
