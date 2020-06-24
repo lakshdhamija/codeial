275ff119b29c7861aca14475c3a7a89c559de402
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-24 10:14:16 +0530

    functionality to destroy posts and comments associated with the post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment')
+
 module.exports.create = function(req, res){
     Post.create({
         content: req.body.content,
@@ -6,5 +8,18 @@ module.exports.create = function(req, res){
     }, function(err, post){
         if(err){console.log('error in creating a post'); return;}
         return res.redirect('back');
+    });
+}
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        if(post.user == req.user.id){ //.id used instead of ._id as .id will convert id to string
+            post.remove();
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
     });
 }
