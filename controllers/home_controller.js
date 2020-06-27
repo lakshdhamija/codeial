fee24ceddd19a004ef3e19ab34c247de485ab5a3
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-27 10:49:52 +0530

    add async await in controllers for better code readability

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,32 +1,26 @@
 const Posts = require('../models/post');
 const User = require('../models/user'); // requiring as we need to display list of users on home page
-module.exports.home = function(req, res){
-    //console.log(req.cookies);
-    //res.cookie('user_id', 25);
-    // Posts.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts: posts
-    //     });
-    // });
-
+module.exports.home = async function(req, res){
     // populate user of each post
-    Posts.find({})
-    .populate('user') // populating user in posts to display user name who made post
-    .populate({
-        path: 'comments', // (name specified according to post.js model) populating comments to display comment
-        populate: {
-            path: 'user' // populating user in comments to display user name who made comment
-        }
-    })
-    .exec(function(err, posts){
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts: posts,
-                all_users: users
-            });
+    try{
+        let posts = await Posts.find({})
+        .populate('user') // populating user in posts to display user name who made post
+        .populate({
+            path: 'comments', // (name specified according to post.js model) populating comments to display comment
+            populate: {
+                path: 'user' // populating user in comments to display user name who made comment
+            }
         });
-        
-    });
+
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts,
+            all_users: users
+        });
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
