cb774a18c16e8696f8794518a978cb962b534858
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-29 13:46:41 +0530

    setup nodeMailer

---
+++ config/nodemailer.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+// defines how communication is going to take place
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: '587',
+    secure: false,
+    auth: {
+        user: 'laksh.dhamija@gmail.com',
+        pass: 'XJJd45N5DfzFaHe'
+    }
+});
+
+// defines location for html email template 
+let renderTemplate = function(data, relativePath){
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function(err, template){
+            if(err){console.log('Error in rendering template: ', err); return;}
+            mailHTML = template;
+        }
+    )
+    return mailHTML;
+}
+
+//exporting the two properties defined above
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
