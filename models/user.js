ba910b86ce7abfae43097b85dc2c89aaf6cc28ea
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-07-02 14:05:33 +0530

    map avatars to users

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -13,10 +17,32 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String
     }
-},{
+}, {
     timestamps: true
 });
+
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..', AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now());
+    }
+  });
+
+
+// static
+userSchema.statics.uploadedAvatar = multer({storage:  storage}).single('avatar'); // attaching properties of iskStorage to multer
+// .single specifies that we will be storing avatar in only 1 file instad of multiple files
+// to access this function -> User.uploadedAvatar
+userSchema.statics.avatarPath = AVATAR_PATH; // making AVATAR_PATH publically accessible -> User.avatarPath
+
+
 
 const User = mongoose.model('User', userSchema);
 
