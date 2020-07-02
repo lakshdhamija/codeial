418b6e3b89ea8797aff115b3d5bf21f95fad965b
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-07-02 15:47:34 +0530

    reder api v2

--- routes/api/v2/index.js
+++ routes/api/v2/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/v1', require('./v1'));
-router.use('/v2', require('./v2'));
+router.use('/comments', require('./comments'));
 
 module.exports = router;
